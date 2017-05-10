8335f357c8eb98eb3574ab750110112aabeb7a4f
Oleg Pudeyev <oleg@bsdpower.com>
2017-05-10 09:33:18 -0400

    Ability to force execution

--- Readme.md
+++ Readme.md
@@ -1,7 +1,8 @@
 
 # debounce
 
-  Useful for implementing behavior that should only happen after a repeated action has completed.
+  Useful for implementing behavior that should only happen after a repeated
+  action has completed.
 
 ## Installation
 
@@ -28,15 +29,32 @@ To later clear the timer and cancel currently scheduled executions:
 window.onresize.clear();
 ```
 
+To force the function to execute immediately and reset the timer for
+any future invocations:
+```
+window.onresize.force();
+```
+
 ## API
 
 ### debounce(fn, wait, [ immediate || false ])
 
-  Creates and returns a new debounced version of the passed function that will postpone its execution until after wait milliseconds have elapsed since the last time it was invoked.
+  Creates and returns a new debounced version of the passed function that
+  will postpone its execution until after wait milliseconds have elapsed
+  since the last time it was invoked.
 
-  Pass `true` for the `immediate` parameter to cause debounce to trigger the function on the leading edge instead of the trailing edge of the wait interval. Useful in circumstances like preventing accidental double-clicks on a "submit" button from firing a second time.
+  Pass `true` for the `immediate` parameter to cause debounce to trigger
+  the function on the leading edge instead of the trailing edge of the wait
+  interval. Useful in circumstances like preventing accidental double-clicks
+  on a "submit" button from firing a second time.
 
-  The debounced function returned also has a property 'clear' that is a function that will clear any scheduled future executions of your function.
+  The debounced function returned has a property 'clear' that is a 
+  function that will clear any scheduled future executions of your function.
+
+  The debounced function returned has a property 'force' that is a 
+  function that will immediately execute the function if execution is scheduled,
+  and reset the execution timer for subsequent invocations of the debounced
+  function.
 
 ## License
 
--- test.js
+++ test.js
@@ -42,3 +42,129 @@ describe('catch issue #3 - Debounced function executing early?', function() {
   })
 
 })
+
+describe('forcing execution', function() {
+
+  // use sinon to control the clock
+  var clock
+
+  beforeEach(function(){
+    clock = sinon.useFakeTimers()
+  })
+
+  afterEach(function(){
+    clock.restore()
+  })
+
+  it('should not execute prior to timeout', function() {
+
+    var callback = sinon.spy()
+
+    // set up debounced function with wait of 100
+    var fn = debounce(callback, 100)
+
+    // call debounced function at interval of 50
+    setTimeout(fn, 100)
+    setTimeout(fn, 150)
+
+    // set the clock to 25 (period of the wait) ticks after the last debounced call
+    clock.tick(175)
+
+    // the callback should not have been called yet
+    expect(callback.callCount).toEqual(0)
+
+  })
+
+  it('should execute prior to timeout when forced', function() {
+
+    var callback = sinon.spy()
+
+    // set up debounced function with wait of 100
+    var fn = debounce(callback, 100)
+
+    // call debounced function at interval of 50
+    setTimeout(fn, 100)
+    setTimeout(fn, 150)
+
+    // set the clock to 25 (period of the wait) ticks after the last debounced call
+    clock.tick(175)
+    
+    fn.force()
+
+    // the callback has been called
+    expect(callback.callCount).toEqual(1)
+
+  })
+
+  it('should not execute again after timeout when forced before the timeout', function() {
+
+    var callback = sinon.spy()
+
+    // set up debounced function with wait of 100
+    var fn = debounce(callback, 100)
+
+    // call debounced function at interval of 50
+    setTimeout(fn, 100)
+    setTimeout(fn, 150)
+
+    // set the clock to 25 (period of the wait) ticks after the last debounced call
+    clock.tick(175)
+    
+    fn.force()
+    
+    // the callback has been called here
+    expect(callback.callCount).toEqual(1)
+    
+    // move to past the timeout
+    clock.tick(225)
+
+    // the callback should have only been called once
+    expect(callback.callCount).toEqual(1)
+
+  })
+
+  it('should not execute on a timer after being forced', function() {
+
+    var callback = sinon.spy()
+
+    // set up debounced function with wait of 100
+    var fn = debounce(callback, 100)
+
+    // call debounced function at interval of 50
+    setTimeout(fn, 100)
+    setTimeout(fn, 150)
+
+    // set the clock to 25 (period of the wait) ticks after the last debounced call
+    clock.tick(175)
+    
+    fn.force()
+    
+    // the callback has been called here
+    expect(callback.callCount).toEqual(1)
+    
+    // schedule again
+    setTimeout(fn, 250)
+    
+    // move to past the new timeout
+    clock.tick(400)
+
+    // the callback should have been called again
+    expect(callback.callCount).toEqual(2)
+
+  })
+
+  it('should not execute when forced if nothing was scheduled', function() {
+
+    var callback = sinon.spy()
+
+    // set up debounced function with wait of 100
+    var fn = debounce(callback, 100)
+
+    fn.force()
+    
+    // the callback should not have been called
+    expect(callback.callCount).toEqual(0)
+
+  })
+
+})
